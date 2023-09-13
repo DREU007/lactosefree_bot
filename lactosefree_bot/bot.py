a9d49469bb37419922a8570ba2579b3fa1fe25da
DREU007 <dreu007@gmail.com>
2023-09-14 01:21:41 +0400

    Update bot.py
    Add help_info() and same handler

--- lactosefree_bot/bot.py
+++ lactosefree_bot/bot.py
@@ -45,7 +45,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=help_msg
@@ -93,12 +93,14 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help_info)
 #    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 #    caps_handler = CommandHandler('caps', caps)
 #    inline_caps_handler = InlineQueryHandler(inline_caps)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(start_handler)
+    application.add_handler(help_handler)
 #    application.add_handler(echo_handler)
 #    application.add_handler(caps_handler)
 #    application.add_handler(inline_caps_handler) 
